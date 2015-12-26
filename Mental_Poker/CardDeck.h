d0a45013bfb97bc1c07f1100a6a65ff27ed57cd6
YuriFA <yurik1452@mail.ru>
2015-12-26 17:08:30 +0300

    FINAL

--- Mental_Poker/CardDeck.h
+++ Mental_Poker/CardDeck.h
@@ -4,16 +4,16 @@ typedef std::vector<unsigned> c_deck;
 
 class CardDeck{
 public:
-	c_deck enc_cards;
-    static const unsigned deckSize;
+	c_deck enc_cards;//зашифрованная колода
+    static const unsigned deckSize;// размер колоды
 	CardDeck();
-	void shuffle(bool = false);
-	void sortDeck(bool = false);
-	void show(bool = false);
-	void encDeck(double, double);
-	void decDeck(double, double);
-	void showAll();
+	void shuffle(bool = false);// перетасовка колоды
+	void sortDeck(bool = false); // сортировка колоды
+	void show(bool = false); // вывод на экран колоды
+	void encDeck(double, double); // шифрование колоды
+	void decDeck(double, double); // дешифрование колоды
+	void showAll(); //вывод на экран всех колод
 private:
-    c_deck vec;
+    c_deck vec; // обычная колода
     void Init();
 };
